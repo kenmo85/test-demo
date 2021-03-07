b1196788686f6ed0fa6df85b471e9236cfccf7f1
mozhenbin <913129763@qq.com>
2021-03-07 11:56:40 +0800

    修改了dev分支的css和js代码

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ window.onload = function(){
     let box = document.getElementsByClassName("box")[0];
     box.addEventListener('click',()=>{
         alert("这是box代码")
+        //新增的dev分支代码
     })
 }
